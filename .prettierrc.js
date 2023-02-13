fc17eb1cca9b6f9d737100c939c9079bab82ebd5
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-13 12:38:21 +0100

    fix linters errors

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+};
